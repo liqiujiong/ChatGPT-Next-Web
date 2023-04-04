cda7dc899f267900cb5ea0f9888fed97bb5bee10
李秋炯 <pelle@live.cn>
2023-04-04 21:38:26 +0800

    卸载 analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import { Analytics } from "@vercel/analytics/react";
 import { Home } from "./components/home";
 
 export default function App() {
   return (
     <>
       <Home />
-      <Analytics />
     </>
   );
 }
