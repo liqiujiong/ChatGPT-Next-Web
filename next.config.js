e7dc42bf862d8e30568347a6426683c0702b9658
李秋炯 <pelle@live.cn>
2023-04-13 02:46:49 +0800

    feat: config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,24 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    }); // 针对 SVG 的处理规则
+    experimental: {
+        appDir: true,
+    },
+    webpack (config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+        }); // 针对 SVG 的处理规则
 
-    return config;
-  }
+        return config;
+    },
+    images: {
+        domains: ["mp.weixin.qq.com", "aitop.lqjhome.cn"],
+    },
 };
 
 if (process.env.DOCKER) {
-  nextConfig.output = 'standalone'
+    nextConfig.output = 'standalone'
 }
 
 module.exports = nextConfig;
